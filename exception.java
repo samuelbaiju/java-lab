0f6de977d6d3a1fb2d25b44bcefc271a7614cb3f
samuelbaiju <124334756+samuelbaiju@users.noreply.github.com>
2023-11-28 13:24:07 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,22 @@
+class exceptionhandling
+{
+	public static void main(String args[])
+	{
+		int res;
+		try{
+			res=25/0;
+			}
+		catch(ArithmeticException e1)
+		{	
+			System.out.println("aro");	
+		}+.' vhnj k
+		try{
+			int a[]=new int[5];
+			a[6]=9;
+		}
+		catch(ArrayIndexOutOfBoundsException e2)
+		{
+			System.out.println("byju kaliadan error");
+		}
+	}
+v}mjg ko m/vh['j u
